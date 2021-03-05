34f8ab37b71b4b2a3872dfaa06355c69167d43f0
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-05 10:33:30 +0900

    修正：prototype投稿後トップページに遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to prototype_path(@prototype)
+      redirect_to root_path
     else
       render :new
     end
